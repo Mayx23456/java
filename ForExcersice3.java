fe23f501e0924fb2182d7692c6d8ec2bc929c391
Mayukh <banerjeemayukh2496@gmail.com>
2024-10-15 14:05:40 +0100

    First Commit

---
+++ ForExcersice3.java
@@ -0,0 +1,34 @@
+// Write a Java program that declares a String with a variable name of your choosing. 
+// Assign your own name as the value of the String. 
+
+// Then output to screen the following information and perform the following operations on the string (using the methods that are provided using the dot operator):
+
+// Print out the string
+// Print out the number of characters in the string
+// Print out the first and second characters in the string
+// Print out the string with the lowercase characters replaced by uppercase characters
+// Print out the string with all occurrences of the letter ‘a’ replaced by ‘%’
+// Print out the position within the string of the first and last occurrences of the character ‘r.
+// (note strings are indexed from zero i.e. zero is the first position.
+
+// For example if the input string is: Darryl Stewart
+
+// Then the output should be:
+// The input String : Darryl Stewart
+// The number of characters in the string : 14
+// The first character in the string is D and the second character in the string is a
+// The string in uppercase : DARRYL STEWART
+// The string with the letter a replaced with % is : D%rryl Stew%rt
+// The character ‘r’ first occurs in position 3
+// The character ‘r’ last occurs in position 13
+
+public class ForExcersice3 {
+    public static void main(String[] args) {
+        String yourName = "Mayukh";
+        System.out.println(yourName.length());
+
+        
+
+    }
+    
+}
